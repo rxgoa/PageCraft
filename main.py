a55c68e826fb920cf81febf009c7bea6b79ad46a
rxgoa <>
2024-03-13 14:14:12 -0300

    Added First book inside collection. Now method calculate based on input ISBN and data from JSON file.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import json
+
+
 class Book:
     def __init__(self):
-        self.title = "The Great Gatsby"
-        self.author = "F. Scott Fitzgerald"
-        self.pages = 218
+        self.books = self.read_books_file()
 
-    def estimate_reading_time(self, book_words_total, words_minute):
-        self.total_minutes = book_words_total / words_minute
+    def __getitem__(self, key):
+        return self.books[key]
+
+    def estimate_reading_time(self, isbn, words_minute):
+        book_info = self.get_book_info(isbn)
+        self.total_minutes = book_info["word_count_aprox"] / words_minute
 
         prepare_time = str(format(self.total_minutes / 60, ".2f")).split(
             "."
@@ -21,9 +26,25 @@ class Book:
 
         return self
 
+    def get_book_info(self, isbn):
+        book_info = self.books[isbn]
+        return book_info
+
+    def read_books_file(self):
+        with open("data/books.json") as file:
+            books_json = json.load(file)
+        return books_json
+
 
 book = Book()
-estimate_reading_time = book.estimate_reading_time(64_000, 250)
+estimate_reading_time = book.estimate_reading_time("eISBN:978-0-553-90033-0", 250)
+hours = str(estimate_reading_time.total_hours).split(".")[0]
+minutes = str(estimate_reading_time.total_hours).split(".")[1]
+
 print(
-    estimate_reading_time.total_hours
+    "Estimated time for reading the book is %s hours and %s minutes." % (hours, minutes)
 )  # 4 hours and 16 minutes for 250 words per minute.
+
+print(
+    book.get_book_info("eISBN:978-0-553-90033-0")
+)  # simple return with the ISBN passed
